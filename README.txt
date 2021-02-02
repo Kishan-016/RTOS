2ab2431289327d44ed73c81417aca02288072f9a
Kishan-016 <78338647+Kishan-016@users.noreply.github.com>
2021-02-02 19:58:45 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+ Here Port Number is given as an argument. So don't forget to pass the port number while running the code.
+ 
+   Follow these steps to run these codes:
+   1. run the server code first: gcc server.c -o server
+   2.  ./server port_number
+   3. run the client code:  gcc client.c -o client
+   4.  ./client IpAddress port_number
---
+++ server.c
@@ -0,0 +1,62 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void error(const char *text)
+{
+    perror(text);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, new_sockfd, port_num;
+     socklen_t client_len;
+     char buff[255];
+     struct sockaddr_in server_addr, client_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("error opening in socket");
+     bzero((char *) &server_addr, sizeof(server_addr));
+     port_num = atoi(argv[1]);
+     server_addr.sin_family = AF_INET;
+     server_addr.sin_addr.s_addr = INADDR_ANY;
+     server_addr.sin_port = htons(port_num);
+     if (bind(sockfd, (struct sockaddr *) &server_addr,
+              sizeof(server_addr)) < 0) 
+              error("error on the binding");
+     listen(sockfd,5);
+     client_len = sizeof(client_addr);
+     new_sockfd = accept(sockfd, 
+                 (struct sockaddr *) &client_addr, 
+                 &client_len);
+     if (new_sockfd < 0) 
+          error("error on the accept");
+     while(1)
+     {
+           bzero(buff,255);
+           n = read(new_sockfd,buff,255);
+           if (n < 0) error("error reading from the socket");
+           printf("client: %s\n",buff);
+          bzero(buff,255);
+          fgets(buff,255,stdin);
+          n = write(new_sockfd,buff,strlen(buff));
+           if (n < 0) error("error writing to the socket");
+           int i=strncmp("Bye" , buff, 3);
+           if(i == 0)
+               break;
+     }
+     close(new_sockfd);
+     close(sockfd);
+     return 0; 
+}
